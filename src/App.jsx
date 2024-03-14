aa35c505bc240e4362fb965eea65f1a12f1c723b
mitundae <peterbrian28@gmail.com>
2024-03-14 21:49:30 +0300

    added Footer />

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 import Header from './components/header/Header'
 import Home from './components/home/Home'
 import About from './components/about/About'
+import Footer from './components/footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
         <Header />
         <Home />
         <About />
+        <Footer />
       </div>
       
     </>
