9714d69152bdb77d284e804f805db84462db141d
mitundae <peterbrian28@gmail.com>
2024-03-14 22:18:20 +0300

    brief description

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -5,10 +5,12 @@ const Home = () => {
     return (
         <div>
             <div className="jumbotron">
-                <h1 className="display-4">Hello, world!</h1>
-                <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                <h1 className="display-4">Hello!</h1>
+                <p className="lead">
+                    My portfolio showcases my expertise in web design, with a strong focus on utilizing Figma to create visually appealing and user-friendly interfaces. With proficiency in backend technologies such as Flask, MySQL, and Python, alongside frontend stacks including Bootstrap 5, React, and JavaScript, I deliver dynamic and responsive websites tailored to meet diverse project requirements.
+                </p>
                 <hr className="my-4" />
-                <p>It uses utility classNamees for typography and spacing to space content out within the larger container.</p>
+                <p>Explore my portfolio to see how I blend design creativity with technical prowess to craft engaging online experiences..</p>
                 <a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
             </div>
         </div>
