cff4df5262f5c98afd95bb189c4b16dc2053fcc1
mitundae <peterbrian28@gmail.com>
2024-03-14 17:07:48 +0300

    created home and header so far

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -13,7 +13,7 @@ const Header = () => {
         <Navbar.Brand href="#home">Mitunda Portfolio</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
+          <Nav className="ms-auto">
             <Nav.Link href="#home">Home</Nav.Link>
             <Nav.Link href="#about">About</Nav.Link>
             <Nav.Link href="#contact">Contact</Nav.Link>
