8ead2f8d1f9d07d7c4190b650bbfa056672a287e
mitundae <peterbrian28@gmail.com>
2024-03-14 17:31:25 +0300

    styled the components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -10,7 +10,7 @@ const Header = () => {
     <div>
       <Navbar bg = "light" expand= "lg">
       <Container>
-        <Navbar.Brand href="#home">Mitunda Portfolio</Navbar.Brand>
+        <Navbar.Brand href="#home"></Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ms-auto">
