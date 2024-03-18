92ad9b09c57e1f776e8ded3f1268f7cf569abf8a
mitundae <peterbrian28@gmail.com>
2024-03-18 19:50:32 +0300

    navbar visibility in smaller screens fixed

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,23 +6,22 @@ import Navbar from 'react-bootstrap/Navbar';
 
 const Header = () => {
   return (
-    <div>
-      <Navbar bg = "light" expand= "lg">
-      <Container>
-        <Navbar.Brand href="#home"></Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
+    <div className="navbar-container">
+      <Navbar bg="dark" expand="lg" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Peter Mitunda</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ms-auto">
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#about">About</Nav.Link>
+              <Nav.Link href="#contact">Contact</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
       </Navbar>
-
     </div>
   )
 }
 
-export default Header
+export default Header;
